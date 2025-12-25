7a12f88b624ca0519d3edc0cb087d0183914f5b1
Ogasawara <kakeru19870423@gmail.com>
2025-12-26 03:44:10 +0900

    fixed env

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,12 @@ import ExploreBtn from "@/components/ExploreBtn";
 import { EventItem } from "@/lib/constants";
 import { cacheLife } from "next/cache";
 
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
 const Page = async () => {
   "use cache";
   cacheLife("hours");
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/events`
-  );
+  const response = await fetch(`${BASE_URL}/api/events`);
   const { events } = await response.json();
 
   return (
